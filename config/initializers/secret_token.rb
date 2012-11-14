cfd74b73e7d649652eb8724dec43c3bc0fc228c4
Vincent Samaco <vsamaco@gmail.com>
2012-11-13 23:13:52 -0800

    messages view rspec testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Messages::Application.config.secret_token = 'e712d907ac1b1a90a3471100673420409cd4b58ee923cf0718a8ff0da9b4d521225259836ce7ba75593d8576b2abc48d945fa720b1c9579bdaf1123ef65004be'
---
+++ spec/views/messages/new.html_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe "messages/new.html.erb" do
+  let(:message) do
+    mock_model("Message").as_new_record.as_null_object
+  end
+  
+  before do
+    assign(:message, message)
+  end
+  
+  it "renders a form to create a message" do
+    render
+    rendered.should have_selector("form",
+      :method => "post",
+      :action => messages_path
+    ) do |form|
+      form.should have_selector("input", :type => "submit")
+    end
+  end
+  
+  it "renders a text field for the message title" do
+    message.stub(:title => "the title")
+    render
+    rendered.should have_selector("form") do |form|
+      form.should have_selector("input",
+        :type => "text",
+        :name => "message[title]",
+        :value => "the title"
+      )
+    end
+  end
+  
+  it "renders a text area for the message text" do
+    message.stub(:text => "the message")
+    render
+    rendered.should have_selector("form") do |form|
+      form.should have_selector("textarea",
+        :name => "message[text]",
+        :content => "the message"
+      )
+    end
+    
+  end
+end
