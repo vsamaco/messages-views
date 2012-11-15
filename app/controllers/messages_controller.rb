6781a4d3f3c37416d2b4e4aa6632ef5c1ab72eae
Vincent Samaco <vsamaco@gmail.com>
2012-11-14 19:55:31 -0800

    setup example when Message#save fails

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,11 @@
 class MessagesController < ApplicationController
   def create
-    message = Message.new(params[:message])
-    if message.save
+    @message = Message.new(params[:message])
+    if @message.save
       flash[:notice] = "The message was saved successfully."
+      redirect_to :action => "index"
+    else
+      render :action => "new"
     end
-    redirect_to :action => "index"
   end
 end
--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -12,12 +12,7 @@ describe MessagesController do
       Message.should_receive(:new).with("text" => "a quick brown fox").and_return(message)
       post :create, :message => { "text" => "a quick brown fox" }
     end
-    
-    it "saves the message" do
-      message.should_receive(:save)
-      post :create
-    end
-    
+
     context "when the message saves successfully" do
       it "sets a flash[:notice] message" do
         post :create
@@ -27,6 +22,20 @@ describe MessagesController do
       it "redirects to the Messages index" do
         post :create
         response.should redirect_to(:action => "index")
+      end
+    end
+    
+    context "when the message fails to save" do
+      it "assigns @message" do
+        message.stub(:save).and_return(false)
+        post :create
+        assigns[:message].should eq(message)
+      end
+      
+      it "renders the new template" do
+        message.stub(:save).and_return(false)
+        post :create
+        response.should render_template("new")
       end
     end
   end
