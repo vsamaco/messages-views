aee6b2e5c66ab98be6c19cd9bea1eeb782f99c79
Vincent Samaco <vsamaco@gmail.com>
2012-11-14 22:17:33 -0800

    created spec stubs for subscription

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe Subscription do
+  describe "#can_send_message?" do
+    context "when a user has not reached the subscription limit for the month" do
+      it "returns true"
+    end
+    
+    context "when a user has reached the subscription limit for the month" do
+      it "returns false"
+    end
+  end
 end
