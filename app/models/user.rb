ca8999a51634aa29a46388502be4f41bff6efb88
Vincent Samaco <vsamaco@gmail.com>
2012-11-14 21:34:50 -0800

    updated User#send_messages use sent_messages relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
   has_many :received_messages, :class_name => Message.name,
     :foreign_key => "recipient_id"
+  has_many :sent_messages, :class_name => Message.name,
+    :foreign_key => "sender_id"
     
   attr_accessible :login
   
   def send_message(message_attributes)
-    Message.create! message_attributes
+    sent_messages.create! message_attributes
   end
 end
