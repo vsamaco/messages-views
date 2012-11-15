1ae5f6b21015db120964de8ef3f9563d7d9dc7b8
Vincent Samaco <vsamaco@gmail.com>
2012-11-14 19:34:14 -0800

    updated Message#create with redirect and new

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
   def create
+    Message.new(params[:message])
+    redirect_to :action => "index"
   end
 end
--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -3,9 +3,14 @@ require 'spec_helper'
 describe MessagesController do
   describe "POST create" do
     it "creates a new message" do
-      Messages.should_receive(:new).with("text" => "a quick brown fox")
+      Message.should_receive(:new).with("text" => "a quick brown fox")
       post :create, :message => { "text" => "a quick brown fox" }
     end
     it "saves the message"
+    
+    it "redirects to the Messages index" do
+      post :create
+      response.should redirect_to(:action => "index")
+    end
   end
 end
