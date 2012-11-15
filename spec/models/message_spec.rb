5f9f81620f9c124ce5e04f4255cf6303c2aacf7b
Vincent Samaco <vsamaco@gmail.com>
2012-11-14 21:03:19 -0800

    implemented message spec examples for validation

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,11 +1,30 @@
 require 'spec_helper'
 
 describe Message do
-  it "is valid with valid attributes"
+  before do
+    @message = Message.new(
+      :title => "foo", 
+      :text => "bar",
+      :recipient => mock_model("User")
+    )
+  end
   
-  it "is not valid without a title"
+  it "is valid with valid attributes" do
+    @message.should be_valid
+  end
   
-  it "is not valid without text"
+  it "is not valid without a title" do
+    @message.title = nil
+    @message.should_not be_valid
+  end
   
-  it "is not valid without a recipient"
+  it "is not valid without text" do
+    @message.text = nil
+    @message.should_not be_valid
+  end
+  
+  it "is not valid without a recipient" do
+    @message.recipient = nil
+    @message.should_not be_valid
+  end
 end
