e51f4278b50e0e65527129091cf16d71f7217fca
Vincent Samaco <vsamaco@gmail.com>
2012-11-14 21:17:57 -0800

    implemented send message spec example

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "#send message" do
+    context "when a the user is under their subscription limit" do
+      it "sends a message to another user" do
+        zach = User.create!
+        david = User.create!
+        msg = zach.send_message(
+          :title => "Book Update",
+          :text => "Beta 11 includes great stuff!",
+          :recipient => david
+        )
+        david.received_messages.should == [msg]
+      end
+      
+      it "creats a new message with the submitted attributes" do
+        zach = User.create!
+        david = User.create!
+        msg = zach.send_message(
+          :title => "Book Update",
+          :text => "Beta 11 includes great stuff!",
+          :recipient => david
+        )
+        msg.title.should == "Book Update"
+        msg.text.should == "Beta 11 includes great stuff!"
+      end
+    end
+    
+  end
 end
