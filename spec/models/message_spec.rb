bce8fa061a9a0b268d452e266270cc0600db2ce2
Vincent Samaco <vsamaco@gmail.com>
2012-11-14 20:51:33 -0800

    defining message model spec examples

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Message do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes"
+  
+  it "is not valid without a title"
+  
+  it "is not valid without text"
+  
+  it "is not valid without a recipient"
 end
