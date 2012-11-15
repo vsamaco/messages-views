e35446b05a797517bf8aa8b901fa56c52f4a4bd7
Vincent Samaco <vsamaco@gmail.com>
2012-11-14 19:16:38 -0800

    defined Messages#create spec

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -2,7 +2,10 @@ require 'spec_helper'
 
 describe MessagesController do
   describe "POST create" do
-    it "creates a new message"
+    it "creates a new message" do
+      Messages.should_receive(:new).with("text" => "a quick brown fox")
+      post :create, :message => { "text" => "a quick brown fox" }
+    end
     it "saves the message"
   end
 end
