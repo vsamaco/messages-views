b71d05e099a89254705d4668fe78a8fbd1577933
Vincent Samaco <vsamaco@gmail.com>
2012-11-14 20:14:42 -0800

    implemented application access denied handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  rescue_from AccessDenied, :with => :access_denied
+  
+protected
+  
+  def access_denied
+    redirect_to "/401.html"
+  end
 end
