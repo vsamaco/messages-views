da96699e5d872782a8e214f17bbb94d2695e97c1
Vincent Samaco <vsamaco@gmail.com>
2012-11-14 21:08:24 -0800

    updated messages#create send message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
   def create
-    @message = Message.new(params[:message])
-    if @message.save
+    @message = current_user.send_message(params[:message])
+    if @message.new_record?
       flash[:notice] = "The message was saved successfully."
       redirect_to :action => "index"
     else
