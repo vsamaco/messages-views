63e2dad251e55beac8ee0b68ce2d904c0186f2e8
Vincent Samaco <vsamaco@gmail.com>
2012-11-14 21:18:54 -0800

    updated User message relationship and implemented send_message()

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
+  has_many :received_messages, :class_name => Message.name,
+    :foreign_key => "recipient_id"
+    
   attr_accessible :login
+  
+  def send_message(message_attributes)
+    Message.create! message_attributes
+  end
 end
